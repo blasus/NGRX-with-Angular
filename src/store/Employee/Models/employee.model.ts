0e2e64f2feeae9562e789bc26d237617224ed595
Francesco <xblasi@hotmail.it>
2024-02-28 09:54:13 +0000

    added bootstrap

--- src/store/Employee/Models/employee.model.ts
+++ src/store/Employee/Models/employee.model.ts
@@ -1,7 +1,18 @@
+import { FormControl } from "@angular/forms";
+
 export interface IEmployee {
   id: string;
   name: string;
+  email: string;
   department: string;
-  salary: string;
+  salary: number;
   role: string;
+}
+
+export interface IEmployeeForm {
+  name: FormControl<string>;
+  email: FormControl<string>;
+  department: FormControl<string>;
+  role: FormControl<string>;
+  salary: FormControl<number>;
 }
